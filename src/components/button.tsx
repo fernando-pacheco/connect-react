6e5a4814323f543692dead8f928dc5cebbe5a46c
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2025-02-19 16:39:12 -0300

    feat: componente de input

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,5 +1,14 @@
-function Button() {
-    return <button>oooi</button>
+import type { ComponentProps } from "react"
+
+interface ButtonProps extends ComponentProps<"button"> {}
+
+function Button(props: ButtonProps) {
+    return (
+        <button
+            className="flex items-center justify-between px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-150"
+            {...props}
+        />
+    )
 }
 
 export { Button }
