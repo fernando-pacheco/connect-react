6fe71a91d5366263a3eeff5511a17ef158ce5315
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2025-02-19 14:40:40 -0300

    chore: primeiras configurações next app

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import "./globals.css"
+import type { Metadata } from "next"
+import { Montserrat, Oxanium } from "next/font/google"
+
+export const metadata: Metadata = {
+    title: "DevStage",
+}
+
+const oxanium = Oxanium({
+    weight: ["500", "600"],
+    subsets: ["latin"],
+    variable: "--font-oxanium",
+})
+
+const montserrat = Montserrat({
+    weight: ["400", "600"],
+    subsets: ["latin"],
+    variable: "--font-montserrat",
+})
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode
+}>) {
+    return (
+        <html lang="en">
+            <body className={`${oxanium.variable} ${montserrat.variable}`}>
+                {children}
+            </body>
+        </html>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/button"
+
+export default function Home() {
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+            <Button />
+        </h1>
+    )
+}
