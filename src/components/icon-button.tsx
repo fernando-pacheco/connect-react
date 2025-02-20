271d4bd4d0650932bb0eddbe862f80f4fe0fe078
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2025-02-20 14:02:07 -0300

    feat: uso do twmerge para concatenar estilizações

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,11 +1,15 @@
 import type { ComponentProps } from "react"
+import { twMerge } from "tailwind-merge"
 
 interface IconButtonProps extends ComponentProps<"button"> {}
 
-function IconButton(props: IconButtonProps) {
+function IconButton({ className, ...props }: IconButtonProps) {
     return (
         <button
-            className="items-center justify-between p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-150"
+            className={twMerge(
+                "items-center justify-between p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-150",
+                className
+            )}
             {...props}
         />
     )
